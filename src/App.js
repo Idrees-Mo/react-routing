83e9bc479105099e3c5201d1f24c369ed04a6ad7
Muhammad Idrees <idris@y7mail.com>
2023-08-17 12:36:04 +1000

    removed unwanted imports

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-  RouterProvider,
-} from "react-router-dom";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 
 // Alternative way of creating router with JSX
-const routeDefinations = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/products" element={<ProductsPage />} />
-  </Route>
-);
-const router = createBrowserRouter(routeDefinations);
+
+// const routeDefinations = createRoutesFromElements(
+//   <Route>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/products" element={<ProductsPage />} />
+//   </Route>
+// );
+// const router = createBrowserRouter(routeDefinations);
 
 // routes
-// const router = createBrowserRouter([
-//   { path: "/", element: <HomePage /> },
-//   { path: "/products", element: <ProductsPage /> },
-// ]);
+
+const router = createBrowserRouter([
+  { path: "/", element: <HomePage /> },
+  { path: "/products", element: <ProductsPage /> },
+]);
 
 // App component
 function App() {
