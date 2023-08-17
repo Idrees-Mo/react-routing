87618b010b6947e006cddfdd39d64737becbe349
Muhammad Idrees <idris@y7mail.com>
2023-08-17 14:33:54 +1000

    useNavigate hook  for programatic navigation

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function ProductsPage() {
-  return <h1>Products Page </h1>;
+  // useNavigate hook to do programatic navigation
+  const navigate = useNavigate();
+
+  const goHome = () => {
+    navigate("/");
+  };
+
+  return (
+    <>
+      <h1>Products Page </h1>
+      <button onClick={goHome}>Go Home </button>
+    </>
+  );
 }
 
 export default ProductsPage;
