4b0ceaa5cfc130292ff277cc06525e50aba43800
Muhammad Idrees <idris@y7mail.com>
2023-08-17 22:56:03 +1000

    relative paths

--- src/App.js
+++ src/App.js
@@ -23,9 +23,9 @@ const router = createBrowserRouter([
     element: <RootLayout />,
     errorElement: <Error />,
     children: [
-      { path: "/", element: <HomePage /> },
-      { path: "/products", element: <ProductsPage /> },
-      { path: "/products/:productId", element: <ProductDetail /> },
+      { path: "", element: <HomePage /> },
+      { path: "products", element: <ProductsPage /> },
+      { path: "products/:productId", element: <ProductDetail /> },
     ],
   },
 ]);
